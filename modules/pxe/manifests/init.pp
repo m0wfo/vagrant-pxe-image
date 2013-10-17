e91aa42599f2ec83f3391c82192c2ece20a1676b
Chris Mowforth <chris@mowforth.com>
2013-10-17 04:12:04 +0100

    got debian netbooting over iPXE

--- modules/pxe/manifests/init.pp
+++ modules/pxe/manifests/init.pp
@@ -1,27 +1,56 @@
 class pxe {
 
+  service { "dnsmasq":
+    ensure  => "running",
+    enable  => "true",
+    require => Package["dnsmasq"]
+  }
+
+  package { "ipxe":
+    ensure => "installed"
+  }
+  ->
+  package { "nginx":
+    ensure => "installed"
+  }
+  ->
   package { "dnsmasq":
     ensure => "installed"
   }
   ->
-  file { ["/tftpboot", "/tftpboot/boot", "/tftpboot/boot/grub", "/tftpboot/platform"]:
+  file { ["/tftpboot"]:
     ensure => "directory",
     owner  => 'root',
     group  => 'root',
     mode   => '0755'
   }
-  -> file { "/tftpboot/boot/grub/menu.lst":
-    ensure => 'present',
-    owner => 'root',
-    group => 'root',
-    mode  => '0644',
-    content => template("pxe/menu.lst")
+  ->
+  exec { "copy ipxe":
+    command => "/bin/cp /usr/lib/ipxe/undionly.kpxe /tftpboot/",
+    creates => "/tftpboot/undionly.kpxe"
   }
   -> file { "/etc/dnsmasq.conf":
+    notify => Service["dnsmasq"],
     ensure => 'present',
     owner => 'root',
     group => 'root',
     mode  => '0644',
     content => template("pxe/dnsmasq.conf")
+  }
+  ->
+  file { "/usr/share/nginx/www/bootstrap.ipxe":
+    ensure => 'present',
+    owner => 'root',
+    group => 'root',
+    mode  => '0644',
+    content => template("pxe/bootstrap.ipxe")
+  }
+  ->
+  file { "/usr/share/nginx/www/menu.ipxe":
+    ensure => 'present',
+    owner => 'root',
+    group => 'root',
+    mode  => '0644',
+    content => template("pxe/menu.ipxe")
   }
 }
